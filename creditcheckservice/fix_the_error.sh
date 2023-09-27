620a866d2777a4ceff3eb616136a4274a71795f6
IsmaelKP2 <ipapa@splunk.com>
2023-09-27 16:18:08 +0100

    tydying + solutions

--- creditcheckservice/fix_the_error.sh
+++
@@ -1,4 +0,0 @@
-#add hostname in the yaml file
-sed -i "s/{{addhost}}/$HOSTNAME/" creditcheckservice-solutions.yaml
-#deploy the pod from credicheckservice-solutions.yaml 
-kubectl apply -f creditcheckservice-solutions.yaml
